82ec7f1d30480afa6fd1aba04b1bc4584d1117c5
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-10-15 17:53:59 -0500

    add letter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import { Row, Col, Card } from 'react-bootstrap'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <h1>Hangman</h1>
+
       
     </>
   )
